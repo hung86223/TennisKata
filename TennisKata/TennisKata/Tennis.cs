5ddc92c1b78d19c17739f5f60f9579aeb91628da
ben.huang52 <ben.huang52@TI03NB62>
2019-06-19 09:43:24 +0800

    Thirty_All

--- TennisKata/TennisKata/Tennis.cs
+++ TennisKata/TennisKata/Tennis.cs
@@ -3,8 +3,18 @@
     public class Tennis
 
     {
+        public int firstplayerscoretime = 0;
+
         public string Score()
         {
+            if (firstplayerscoretime == 0)
+            {
+                return "Love_All";
+            }
+            if (firstplayerscoretime == 1)
+            {
+                return "Thirty_All";
+            }
             return "Love_All";
         }
     }
--- TennisKata/TennisKata/TennisTest.cs
+++ TennisKata/TennisKata/TennisTest.cs
@@ -6,12 +6,21 @@ namespace TennisKata
     [TestClass]
     public class TennisTest
     {
+        private readonly Tennis _tennis = new Tennis();
+
         [TestMethod]
         public void Love_All()
         {
-            var tennis = new Tennis();
-            var score = tennis.Score();
+            var score = _tennis.Score();
             Assert.AreEqual("Love_All", score);
+        }
+
+        [TestMethod]
+        public void Thirty_All()
+        {
+            _tennis.firstplayerscoretime++;
+            var score = _tennis.Score();
+            Assert.AreEqual("Thirty_All", score);
         }
     }
 }
